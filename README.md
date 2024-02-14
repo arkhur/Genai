288b9bf66f8ce34a468b26190bbd44dfc541edd4
Arunav <42786591+arkhur@users.noreply.github.com>
2024-02-14 09:41:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Chatbot
+# st2
---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+st.set_page_config(page_title="WaterCorp's Chatbot!", page_icon="", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+st.title("WaterCorp's Chatbot💬")
+st.info("Available at(https://www.watercorporation.com.au/About-us/Our-performance/Annual-report)", icon="📃")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about our Annual Report!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the doc – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are in the panel of Minister for Water. You are being asked questions about an annual report. Keep your answers technical and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are a College Professor or Corporate Trainer and your job is to answer questions about the course. Assume that all questions are related to the course and documents provided. Keep your answers technical and based on facts – do not hallucinate features.")
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
